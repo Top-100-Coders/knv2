4b4289b0bf8a889a6673b9011153aaebb224d604
Govind-S-B <b.s.dnivog@gmail.com>
2023-11-19 15:25:20 +0530

    added delays and reorder process launch order

--- launch_all_process.bat
+++ launch_all_process.bat
@@ -1,9 +1,10 @@
 start cmd.exe /k "cd beckn_implementation\BAP && docker-compose up"
 start cmd.exe /k "cd beckn_implementation\BPP && docker-compose up"
-start cmd.exe /k "npx lt --port 5002 -s knv2-bap"
-start cmd.exe /k "npx lt --port 5012 -s knv2-bpp"
-timeout /t 10 /nobreak
+timeout /t 40 /nobreak
 start cmd.exe /k "cd beckn_implementation\BAP\client && npm run dev"
 start cmd.exe /k "cd beckn_implementation\BAP\network && npm run dev"
 start cmd.exe /k "cd beckn_implementation\BPP\client && npm run dev"
 start cmd.exe /k "cd beckn_implementation\BPP\network && npm run dev"
+timeout /t 10 /nobreak
+start cmd.exe /k "npx lt --port 5002 -s knv2-bap"
+start cmd.exe /k "npx lt --port 5012 -s knv2-bpp"
